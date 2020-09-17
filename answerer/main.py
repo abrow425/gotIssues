abe563e5ab6e3405f512fab2cbc29f8e86fcb82e
Andrew <50899169+abrow425@users.noreply.github.com>
2020-09-17 11:18:18 +1000

    Update main.py

--- answerer/main.py
+++ answerer/main.py
@@ -16,13 +16,16 @@
 """
 
 import csv
-from time import time
 from nspywrapper import nsRequests
+from time import time
 from random import choice
 # from selenium.webdriver import Firefox
 # from selenium.webdriver.firefox.options import Options
+# from bs4 import BeautifulSoup
 
 __browser__ = False
+ALLOWABLE = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11",
+             "12", "13", "14", "15", "16", "17", "18", "19", "20", "!")
 
 
 def answer_issues():
@@ -31,12 +34,12 @@ def answer_issues():
     # setup
     main_nation_name = str(input("Name of your main nation: "))
     assert main_nation_name not in ("", " ", None)
-    nsapi = nsRequests("1:1 issue answering script by SherpDaWerp, in use by " + main_nation_name)
+    nsapi = nsRequests("manual issue answering script-thing by SherpDaWerp, in use by " + main_nation_name)
 
     # import files
     auth = []
     try:
-        with open("./code/puppets.csv") as csv_file:
+        with open("./puppets.csv") as csv_file:
             csv_reader = csv.reader(csv_file)
             for row in csv_reader:
                 auth.append([row[0].replace(" ", "_"), row[1]])
@@ -46,7 +49,7 @@ def answer_issues():
 
     priorities = []
     try:
-        with open("./code/priorities.csv") as csv_file:
+        with open("./priorities.csv") as csv_file:
             csv_reader = csv.reader(csv_file)
             for row in csv_reader:
                 priorities.append([row[0], row[1]])
@@ -54,11 +57,8 @@ def answer_issues():
         print("Oh no!")
         print(err)
 
-    try:
-        with open("./code/output.txt", "w") as file:
-            file.write("")
-    except (OSError, FileNotFoundError, IOError):
-        pass
+    with open("./output.txt", "w+") as file:
+        file.write("")
 
     # actual code
     for credentials in auth:
@@ -80,13 +80,17 @@ def answer_issues():
                 except ValueError:
                     pass
 
-                if chosen_op is None:
+                if chosen_op is None or chosen_op not in ALLOWABLE:
                     chosen_op = choice(issue_options)
 
-                out_url = f"https://www.nationstates.net/page=enact_dilemma/choice-{chosen_op}=1/dilemma={issue_id}/" \
-                          f"asnation={credentials[0]}/container={credentials[0]}/nation={credentials[0]}/\n"
+                if chosen_op != "!":
+                    out_url = f"https://www.nationstates.net/page=enact_dilemma/choice-{chosen_op}=1/dilemma={issue_id}/" \
+                              f"asnation={credentials[0]}/container={credentials[0]}/nation={credentials[0]}/\n"
+                else:
+                    out_url = f"https://www.nationstates.net/page=show_dilemma/dilemma={issue_id}/" \
+                              f"asnation={credentials[0]}/container={credentials[0]}/nation={credentials[0]}/\n"
 
-                with open("./code/output.txt", "a+") as file:
+                with open("./output.txt", "a+") as file:
                     file.write(out_url)
 
         print(credentials[0])
