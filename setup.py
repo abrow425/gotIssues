175a0d0c226af3418307136057a00fec143d738e
Andrew <50899169+abrow425@users.noreply.github.com>
2020-09-17 14:40:39 +1000

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
     name="answerer",
     packages=["answerer"],
-    version="0.1.3",
+    version="0.1.4",
     license="Apache",
     description="A system that uses containers and scripts to answer issues at *high* speeds.",
     long_description="See here for more information: https://github.com/abrow425/gotIssues",
     author="Andrew Brown (aka SherpDaWerp)",
     author_email="abrow425@gmail.com",
     url="https://github.com/abrow425/gotIssues",
-    download_url="https://github.com/abrow425/gotIssues/archive/v0.1.3.tar.gz",
+    download_url="https://github.com/abrow425/gotIssues/archive/v0.1.4.tar.gz",
     keywords=["NATIONSTATES", "NS", "API"],
     classifiers=[
         "Development Status :: 4 - Beta",
