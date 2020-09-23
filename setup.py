c55d383196772d30732d6994d352a2989ddeed0c
Andrew <50899169+abrow425@users.noreply.github.com>
2020-09-23 17:33:19 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
     name="answerer",
     packages=["answerer"],
-    version="2.0.1",
+    version="2.0.2",
     license="Apache",
     description="A system that outputs a nice list of links to speed up issues answering.",
     long_description="See here for more information: https://github.com/abrow425/gotIssues",
     author="Andrew Brown (aka SherpDaWerp)",
     author_email="abrow425@gmail.com",
     url="https://github.com/abrow425/gotIssues",
-    download_url="https://github.com/abrow425/gotIssues/archive/v2.0.1.tar.gz",
+    download_url="https://github.com/abrow425/gotIssues/archive/v2.0.2.tar.gz",
     keywords=["NATIONSTATES", "NS", "API"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
