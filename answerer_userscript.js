20a8a6be64ea3e0d5193afd99efee8de0799344b
Andrew <50899169+abrow425@users.noreply.github.com>
2020-09-23 17:30:10 +1000

    Update answerer_userscript.js

--- answerer_userscript.js
+++ answerer_userscript.js
@@ -2,21 +2,36 @@
 // @name         answerer_script
 // @namespace    https://github.com/abrow425/gotIssues
 // @version      0.0.1
-// @description  handles output.html links and enforces simultaneity
+// @description  handles issue_link_output.html links and enforces simultaneity
 // @author       SherpDaWerp
-// @include			 file:/*/issue_link_output.html*
-// @include			 https://www.nationstates.net/container=*/page=enact_dilemma/choice-*=1/dilemma=*/template-overall=none/nation=*/asnation=*
-// @include			 http://www.nationstates.net/container=*/page=enact_dilemma/choice-*=1/dilemma=*/template-overall=none/nation=*/asnation=*
-// @include			 https://www.nationstates.net/page=deck
-// @grant				 GM_setValue
-// @grant				 GM_getValue
-// @grant				 window.close
+// @include      file:/*/issue_link_output.html*
+// @include      https://www.nationstates.net/container=*/page=enact_dilemma/choice-*=1/dilemma=*/template-overall=none/nation=*/asnation=*
+// @include      http://www.nationstates.net/container=*/page=enact_dilemma/choice-*=1/dilemma=*/template-overall=none/nation=*/asnation=*
+// @include      https://www.nationstates.net/page=deck
+// @grant        GM.setValue
+// @grant        GM.getValue
+// @grant        window.close
 // ==/UserScript==
 
-function fn_answer(event) {
-  	if (GM.getValue("answerer_is_ns_issue_tab_open") == "true") {
+// function courtesy of https://gomakethings.com/getting-all-query-string-values-from-a-url-with-vanilla-js/
+var getParams = function (url) {
+	var params = {};
+	var parser = document.createElement('a');
+	parser.href = url;
+	var query = parser.search.substring(1);
+	var vars = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+		params[pair[0]] = decodeURIComponent(pair[1]);
+	}
+	return params;
+};
+
+async function fn_answer(event) {
+  	console.log(await GM.getValue("answerer_is_ns_issue_tab_open"));
+  	if (await GM.getValue("answerer_is_ns_issue_tab_open") == "true") {
       	alert("To enforce NationStates' simultaneity rules, you can only open one tab at a time using this script.");
-    } else if (GM.getValue("answerer_is_ns_issue_tab_open") == "false") {
+    } else if (await GM.getValue("answerer_is_ns_issue_tab_open") == "false") {
         var url = this.value;
 
         GM.setValue("answerer_is_ns_issue_tab_open", "true");
@@ -44,7 +59,6 @@ function closeAndResetTab() {
 
 function handleIssueTab() {
     if (document.getElementById("dlegislationbox") !== null) {
-        // if the script goes to an *issue* page
         if (document.getElementsByClassName("button lootboxbutton").length > 0) {
             // yes packs, so focus the "open packs" button ready to be clicked.
 
@@ -65,29 +79,46 @@ function handleIssueTab() {
             }, false);
         } else {
             // no packs, so close tab and allow another to be opened.
-
-            closeAndResetTab();
+            setTimeout(closeAndResetTab, 60000);
         }
     } else {
-        // if the script goes to a *login* page
-        return;
+        // script goes to login page.
+
+        var parameters = getParams(window.location.href)
+        nation_name = parameters["asnation"]
+        token = parameters["t"]
+
+        document.cookie = "autologin=".concat(encodeURIComponent(nation_name, "=", token));
+        location.reload();
     }
 }
 
 function cardflipwaiter() {
-  	// check if there are 5 flipped or 5 junked cards.
+  	var cards = document.getElementsByClassName("ready");
+  	for (i = 0; i < cards.length; i++) {
+  	    if (cards.item(i).className.includes("flipped") && !(cards.item(i).className.includes("junked"))) {
+            // if the back is showing (flipped) and it's not junked, then it hasn't been clicked yet.
 
-  	setTimeout(cardflipwaiter, 50);
+  	        setTimeout(cardflipwaiter, 100);
+  	    }
+  	}
 }
 
+function flipAllCards() {
+    var card_backs = document.getElementsByClassName("back")
 
-(function() {
+    for (i = 0; i < 5; i++) {
+        card_backs.item(i).click()
+    }
+}
+
+(async function() {
   	'use strict';
 
     if (window.location.href.includes("/issue_link_output.html")) {
       	// if on generated page, add fn_answer() to all buttons and notify the user that the script is ready to go.
-        GM.setValue("answerer_pack_to_be_opened", "false");
-        // GM.setValue("answerer_is_ns_issue_tab_open", "false");
+      	GM.setValue("answerer_pack_to_be_opened", "false");
+        GM.setValue("answerer_is_ns_issue_tab_open", "false");
 
 
         addBtnFn();
@@ -97,12 +128,9 @@ function cardflipwaiter() {
       	// if on issue page, wait for page to load and add handleIssueTab() function.
 
       	window.addEventListener("load", handleIssueTab, false);
-    } else if (window.location.href.includes("https://www.nationstates.net/page=deck") && GM.getValue("pack_to_be_opened") == "true") {
-     		if (GM.getValue("answerer_autoflip") == "true") {
-          	var card_backs = document.getElementsByClassName("back")
-          	for (i = 0; i < 5; i++) {
-              	card_backs.item(i).click()
-            }
+    } else if (window.location.href.includes("https://www.nationstates.net/page=deck") && await GM.getValue("pack_to_be_opened") == "true") {
+     	if (await GM.getValue("answerer_autoflip") == "true") {
+     	    window.addEventListener("load", flipAllCards, false);
 
           	setTimeout(closeAndResetTab, 2000);
         } else {
